f7b0c95187a8231184db35268207a59479666298
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-08 15:55:12 -0400

    Add routes and nested resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :new, :show] do 
+    resources :sends
+  end 
+  resources :problems, only: [:index, :show, :new, :create]
+  # resources :walls, only: :create
+  # resources :styles, only: :create
+  # resources :problem_styles, only: :create
+  resources :rewards, only: [:index, :show, :new, :create]
 end
