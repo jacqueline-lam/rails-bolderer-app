c6a483eba6fc72514fbf1826534490a634c82436
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-08 15:57:53 -0400

    Creeate controller #index, new, create, show actions for ProblemsController; define strong params method

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,3 +1,27 @@
 class ProblemsController < ApplicationController
+  def index
+    @problems = Problem.problems_by_date
+  end
+  
+  def new
+    @problem = Problem.new
+  end
 
+  def create
+    @problem = Problem.new(problem_params)
+    if @problem.save
+      redirect_to problems_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def problem_params
+    params.require(:problem).permit(:wall_id, :name, :color, :grade, :image, :style_ids:[])
+  end
 end
