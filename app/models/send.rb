69d024c1204b27be4dbb77c0cadfcae458516c37
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-10 13:47:04 -0400

    First pass at Sends validations; Add 'pry' gem to Gemfile

--- app/models/send.rb
+++ app/models/send.rb
@@ -1,4 +1,15 @@
 class Send < ApplicationRecord
   belongs_to :user
   belongs_to :problem
+
+  validates :attempts, presence: true, numericality: {greater_than: 0}
+  validate :valid_date? 
+  validates_presence_of :user_id, :problem_id
+
+  def valid_date?
+    if date_sent > self.problem.created_at
+      errors.add(:date_sent, "Sent date must be after problem set date")
+    end
+  end
+  
 end
