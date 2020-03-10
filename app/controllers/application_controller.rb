a5805bcfc44315f1c728bcf067ca41ac79f9caa0
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-09 20:09:05 -0400

    Add link to add new send, and define helper methods for all views for user authroization purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  helper_methods :logged_in, :current_user, :owner_of_sends
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !!curent_user
+  end
+
+  def require_login
+    redirect_to :root unless logged_in?
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def owner_of_sends?(user)
+    current_user.id == user.id
+  end
+
 end
