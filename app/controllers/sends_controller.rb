8a0651bd6c7c9e53c6a437fae6d8f6626a8b0843
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-10 00:49:12 -0400

    Create before_action filter for sends#show #update and #delete actions

--- app/controllers/sends_controller.rb
+++ app/controllers/sends_controller.rb
@@ -2,14 +2,15 @@ class SendsController < ApplicationController
   # call to the ActionController class method before_action
   # register a filter 
   before_action :get_user, only:[:index, :show, :new]
+  before_action :get_send, only:[:show, :update, :delete]
 
   def index
-    @sends = get_user.sort_user_sends_by_date
+    @sends = @user.sort_user_sends_by_date
     # @sends = @user.sends
   end
   
   def show
-    @send = Send.find_by(params[:id])
+    
   end
 
   def new
@@ -32,5 +33,10 @@ class SendsController < ApplicationController
   def get_user
     # returns @user or find user and set the instance variable 
     @user ||= User.find_by(params[:user_id])
+  end
+
+  def get_send
+    # returns @user or find user and set the instance variable 
+    @send ||= Send.find_by(params[:id])
   end
 end
