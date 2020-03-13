f8ae5c37aa834f8b028756540447fd8d104d65b5
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-13 16:06:42 -0400

    Add links to nav bar in layouts/application; Fix User instance method 'hardest_send' to return 'no sends' if user has not logged any sends; Add CSS and JS files

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,14 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
   # query user table for user who climbed the most hardest graded problem 
   scope :best_climber, -> { joins(:problems).order('grade desc').distinct.limit(1).first } 
-  
+
   def hardest_send
-    self.problems.order('grade desc').limit(1).first.grade
+    user_problems = self.problems
+    if user_problems.any?
+      return user_problems.order('grade desc').limit(1).first.grade 
+    else
+      return "#{self.username} has not logged any sends." 
+    end
   end
 
   def sort_user_sends_by_date
